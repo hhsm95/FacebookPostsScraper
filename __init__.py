85cf6ffa823838a651c9f6615a8e97ec19e9ccbb
Felipe Mateus <eu@felipemateus.com>
2021-01-05 20:41:26 -0300

    Update

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from FacebookPostsScraper import FacebookPostsScraper
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="facebook_scraper", # Replace with your own username
+    name="facebook-scraper", # Replace with your own username
     version="0.0.1",
     author="Felipe Mateus",
     author_email="eu@felipemateus.com",
